d88f7206224c928576e88ebac0aa40b05e25a65e
mkarbach <moritz.karbach@gmail.com>
2014-10-16 15:18:10 +0200

    adding combiners.cmake

---
+++ cmake/combiners.cmake
@@ -0,0 +1,21 @@
+
+###############################################
+#
+# Define your combiner modules
+#
+# Each combiner consists of a subdirectory of
+# that name, that holds all PDF classes and
+# executables. Take inspiration in the tutorial
+# example.
+#
+###############################################
+
+SET( COMBINER_MODULES
+	tutorial
+	#gammacombo
+	#hfag
+	bsmumu
+	#biggammacombo
+	# add your combiners here
+)
+
