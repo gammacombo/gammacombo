fa3955dc9301cbd477adc0e49cea5b59534b9f8e
mkarbach <moritz.karbach@gmail.com>
2015-01-13 23:25:26 +0100

    indent

--- core/include/PDF_Abs.h
+++ core/include/PDF_Abs.h
@@ -49,89 +49,89 @@ using namespace Utils;
 
 class PDF_Abs
 {
-public:
-                      PDF_Abs(int nObs);
-                      PDF_Abs(int nObs, ParametersAbs &pars);
-  virtual             ~PDF_Abs();
-	virtual void				build();
-  virtual void        buildPdf();
-  void                buildCov();
-  virtual bool        checkConsistency();
-  void                deleteToys(){delete toyObservables;};
-  TString             getBaseName();
-	inline int					getGcId(){return gcId;}
-  inline TString      getName(){return name;};
-  inline int          getNobs(){return nObs;};
-  inline TString      getUniqueID(){return uniqueID;};
-  inline unsigned long long getUniqueGlobalID(){return uniqueGlobalID;}
-  inline RooArgList*  getObservables(){return observables;};
-  inline RooArgList*  getParameters(){return parameters;};
-  inline RooAbsPdf*   getPdf(){return pdf;};
-  inline RooArgList*  getTheory(){return theory;};
-  inline TString      getTitle(){return title;};
-  bool 	              hasObservable(TString obsname);
-	inline bool					isCrossCorPdf(){return m_isCrossCorPdf;}
-  virtual void        initParameters();
-  virtual void        initRelations();
-  virtual void        initObservables();
-  void                loadExtParameters(RooFitResult *r);
-  void                print() const;
-  void                printParameters();
-  void                printObservables();
-  bool                ScaleError(TString obsname, float scale);
-	inline void					setErrorSourceString(TString source){obsErrSource=source;};
-	inline void					setGcId(int id){gcId=id;};
-  void                setObservable(TString name, float value);
-  virtual void        setObservables(TString c);
-  void                setObservablesTruth();
-  void                setObservablesToy();
-	inline void					setObservableSourceString(TString source){obsValSource=source;};
-  inline void         setTitle(TString t){title=t;};
-  virtual void        setUncertainties(TString c);
-  virtual void        setCorrelations(TString c);
-  void                setSystCorrelation(TMatrixDSym &corSystMatrix);
-  void                storeErrorsInObs();
-  void                resetCorrelations();
-  virtual bool        test();
-  void                uniquify(int uID);  ///< used to uniquify all names when added
+	public:
+		PDF_Abs(int nObs);
+		PDF_Abs(int nObs, ParametersAbs &pars);
+		virtual             ~PDF_Abs();
+		virtual void				build();
+		virtual void        buildPdf();
+		void                buildCov();
+		virtual bool        checkConsistency();
+		void                deleteToys(){delete toyObservables;};
+		TString             getBaseName();
+		inline int					getGcId(){return gcId;}
+		inline TString      getName(){return name;};
+		inline int          getNobs(){return nObs;};
+		inline TString      getUniqueID(){return uniqueID;};
+		inline unsigned long long getUniqueGlobalID(){return uniqueGlobalID;}
+		inline RooArgList*  getObservables(){return observables;};
+		inline RooArgList*  getParameters(){return parameters;};
+		inline RooAbsPdf*   getPdf(){return pdf;};
+		inline RooArgList*  getTheory(){return theory;};
+		inline TString      getTitle(){return title;};
+		bool 	              hasObservable(TString obsname);
+		inline bool					isCrossCorPdf(){return m_isCrossCorPdf;}
+		virtual void        initParameters();
+		virtual void        initRelations();
+		virtual void        initObservables();
+		void                loadExtParameters(RooFitResult *r);
+		void                print() const;
+		void                printParameters();
+		void                printObservables();
+		bool                ScaleError(TString obsname, float scale);
+		inline void					setErrorSourceString(TString source){obsErrSource=source;};
+		inline void					setGcId(int id){gcId=id;};
+		void                setObservable(TString name, float value);
+		virtual void        setObservables(TString c);
+		void                setObservablesTruth();
+		void                setObservablesToy();
+		inline void					setObservableSourceString(TString source){obsValSource=source;};
+		inline void         setTitle(TString t){title=t;};
+		virtual void        setUncertainties(TString c);
+		virtual void        setCorrelations(TString c);
+		void                setSystCorrelation(TMatrixDSym &corSystMatrix);
+		void                storeErrorsInObs();
+		void                resetCorrelations();
+		virtual bool        test();
+		void                uniquify(int uID);  ///< used to uniquify all names when added
 
-  // covariance
-  TMatrixDSym covMatrix;
-  TMatrixDSym corMatrix;
-  TMatrixDSym corStatMatrix;
-  TMatrixDSym corSystMatrix;
-  vector<double> StatErr;
-  vector<double> SystErr;
-  TString corSource;
-    
-protected:
-	TString obsValSource;
-  TString obsErrSource;
-  
-  void                    addToTrash(TObject*);
-	RooArgList*             parameters;   // holds all fit parameters of this PDF
-  RooArgList*             theory;       // holds all truth relations
-	RooArgList*             observables;  // holds all observables
-  TString                 name;
-  TString                 title;        // to be printed in human readable summaries
-  RooAbsPdf*              pdf;          // the PDF
-  int                     nObs;         // number of observables
-  map<string,TObject*>    trash;        // trash bin, gets emptied in destructor
-	bool										m_isCrossCorPdf;	// Cross correlation PDFs need some extra treatment in places, e.g. in uniquify()
+		// covariance
+		TMatrixDSym covMatrix;
+		TMatrixDSym corMatrix;
+		TMatrixDSym corStatMatrix;
+		TMatrixDSym corSystMatrix;
+		vector<double> StatErr;
+		vector<double> SystErr;
+		TString corSource;
 
-  // The following three members are to gain performance during
-  // toy generation - generating 1000 toys is much faster than 1000 times one toy.
-  int                     nToyObs;        // Number of toy observables to be pregenerated.
-  RooDataSet*             toyObservables; // A dataset holding nToyObs pregenerated toy observables.
-  int                     iToyObs;        // Index of next unused set of toy observables.
-	int											gcId;						// ID of this PDF inside a GammaCombo object. Used to refer to this PDF.
-  
-private:
-	void											printCorMatrix(TString title, TString source, const TMatrixDSym& cor) const;
-  TString                   uniquifyThisString(TString s, int uID);
-  TString                   uniqueID;       	// see also uniquify()
-  static unsigned long long counter;        	// Counts the total number of PDF_Abs objects that are created.
-  unsigned long long        uniqueGlobalID; 	// Used only in the Combiner::delPdf() mechanism. Uses the counter, so will depend on creation order.
+	protected:
+		TString obsValSource;
+		TString obsErrSource;
+
+		void                    addToTrash(TObject*);
+		RooArgList*             parameters;   // holds all fit parameters of this PDF
+		RooArgList*             theory;       // holds all truth relations
+		RooArgList*             observables;  // holds all observables
+		TString                 name;
+		TString                 title;        // to be printed in human readable summaries
+		RooAbsPdf*              pdf;          // the PDF
+		int                     nObs;         // number of observables
+		map<string,TObject*>    trash;        // trash bin, gets emptied in destructor
+		bool										m_isCrossCorPdf;	// Cross correlation PDFs need some extra treatment in places, e.g. in uniquify()
+
+		// The following three members are to gain performance during
+		// toy generation - generating 1000 toys is much faster than 1000 times one toy.
+		int                     nToyObs;        // Number of toy observables to be pregenerated.
+		RooDataSet*             toyObservables; // A dataset holding nToyObs pregenerated toy observables.
+		int                     iToyObs;        // Index of next unused set of toy observables.
+		int											gcId;						// ID of this PDF inside a GammaCombo object. Used to refer to this PDF.
+
+	private:
+		void											printCorMatrix(TString title, TString source, const TMatrixDSym& cor) const;
+		TString                   uniquifyThisString(TString s, int uID);
+		TString                   uniqueID;       	// see also uniquify()
+		static unsigned long long counter;        	// Counts the total number of PDF_Abs objects that are created.
+		unsigned long long        uniqueGlobalID; 	// Used only in the Combiner::delPdf() mechanism. Uses the counter, so will depend on creation order.
 };
 
 #endif
