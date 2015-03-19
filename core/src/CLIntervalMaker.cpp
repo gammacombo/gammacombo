e50c2a4a4efb5d22ed466f97226e06d9c7f94963
mkarbach <moritz.karbach@gmail.com>
2015-03-19 11:02:38 +0100

    indent

--- core/src/CLIntervalMaker.cpp
+++ core/src/CLIntervalMaker.cpp
@@ -1,6 +1,6 @@
 #include "CLIntervalMaker.h"
 
-CLIntervalMaker::CLIntervalMaker(OptParser *arg, const TH1F &pvalues)
+	CLIntervalMaker::CLIntervalMaker(OptParser *arg, const TH1F &pvalues)
 : _pvalues(pvalues)
 {
 	assert(arg);
@@ -50,7 +50,7 @@ void CLIntervalMaker::findMaxima(float pValueThreshold)
 {
 	for ( int i=2; i<_pvalues.GetNbinsX()-1; i++ ){
 		if ( _pvalues.GetBinContent(i-1) < _pvalues.GetBinContent(i)
-			&& _pvalues.GetBinContent(i)   > _pvalues.GetBinContent(i+1) ){
+				&& _pvalues.GetBinContent(i)   > _pvalues.GetBinContent(i+1) ){
 			if ( _pvalues.GetBinContent(i) > pValueThreshold ){
 				CLInterval cli;
 				cli.centralmethod = "max bin";
@@ -101,27 +101,27 @@ void CLIntervalMaker::findRawIntervals(float pvalue, vector<CLInterval> &clis) c
 		clis[i].maxmethod = "bins";
 		int centralValueBin = valueToBin(clis[i].central);
 
-	  // find lower interval bound
+		// find lower interval bound
 		clis[i].min = _pvalues.GetXaxis()->GetXmin();
-    for ( int j=centralValueBin; j>0; j-- ){
-    	if ( _pvalues.GetBinContent(j) < pvalue ){
+		for ( int j=centralValueBin; j>0; j-- ){
+			if ( _pvalues.GetBinContent(j) < pvalue ){
 				clis[i].min = _pvalues.GetBinCenter(j);
-        break;
-      }
-   	}
+				break;
+			}
+		}
 
-	  // find upper interval bound
+		// find upper interval bound
 		clis[i].max = _pvalues.GetXaxis()->GetXmax();
-    for ( int j=centralValueBin; j<_pvalues.GetNbinsX(); j++ ){
-    	if ( _pvalues.GetBinContent(j) < pvalue ){
+		for ( int j=centralValueBin; j<_pvalues.GetNbinsX(); j++ ){
+			if ( _pvalues.GetBinContent(j) < pvalue ){
 				clis[i].max = _pvalues.GetBinCenter(j);
-        break;
-      }
-   	}
+				break;
+			}
+		}
 
 		// check if both boundaries were found
 		clis[i].closed = ( clis[i].min != _pvalues.GetXaxis()->GetXmin()
-			              && clis[i].max != _pvalues.GetXaxis()->GetXmax() );
+				&& clis[i].max != _pvalues.GetXaxis()->GetXmax() );
 	}
 }
 
@@ -133,7 +133,7 @@ void CLIntervalMaker::findRawIntervals(float pvalue, vector<CLInterval> &clis) c
 void CLIntervalMaker::removeBadIntervals()
 {
 	vector<CLInterval> _clintervals1sigmaTmp;
-  vector<CLInterval> _clintervals2sigmaTmp;
+	vector<CLInterval> _clintervals2sigmaTmp;
 	for ( int i=0; i<_clintervals1sigma.size(); i++ ){
 		if ( _clintervals1sigma[i].pvalue>0 ) _clintervals1sigmaTmp.push_back(_clintervals1sigma[i]);
 	}
@@ -153,7 +153,7 @@ void CLIntervalMaker::removeBadIntervals()
 bool CLIntervalMaker::binsOnSameSide(int i, float y) const
 {
 	return (  (_pvalues.GetBinContent(i)>y && _pvalues.GetBinContent(i+1)>y)
-         || (_pvalues.GetBinContent(i)<y && _pvalues.GetBinContent(i+1)<y) );
+			|| (_pvalues.GetBinContent(i)<y && _pvalues.GetBinContent(i+1)<y) );
 }
 
 int CLIntervalMaker::checkNeighboringBins(int i, float y) const
@@ -179,16 +179,16 @@ int CLIntervalMaker::checkNeighboringBins(int i, float y) const
 bool CLIntervalMaker::interpolateLine(const TH1F* h, int i, float y, float &val) const
 {
 	// cout << "CLIntervalMaker::interpolateLine(): i=" << i << " y=" << y << endl;
-  if ( !( 1 <= i && i <= h->GetNbinsX()-1 ) ) return false;
+	if ( !( 1 <= i && i <= h->GetNbinsX()-1 ) ) return false;
 	if ( binsOnSameSide(i, y) ) {
 		cout << "CLIntervalMaker::interpolateLine() : ERROR : bins i and i+1 on same side of y" << endl;
 		return false;
 	}
-  float p1x = h->GetBinCenter(i);
+	float p1x = h->GetBinCenter(i);
 	float p1y = h->GetBinContent(i);
-  float p2x = h->GetBinCenter(i+1);
-  float p2y = h->GetBinContent(i+1);
-  val = p2x + (y-p2y)/(p1y-p2y)*(p1x-p2x);
+	float p2x = h->GetBinCenter(i+1);
+	float p2y = h->GetBinContent(i+1);
+	val = p2x + (y-p2y)/(p1y-p2y)*(p1x-p2x);
 	return true;
 }
 
@@ -252,8 +252,8 @@ void CLIntervalMaker::improveIntervalsPol2fit(vector<CLInterval> &clis) const
 ///
 float CLIntervalMaker::pq(float p0, float p1, float p2, float y, int whichSol) const
 {
-  if ( whichSol == 0 ) return -p1/2./p2 + sqrt( sq(p1/2./p2) - (p0-y)/p2 );
-  else                 return -p1/2./p2 - sqrt( sq(p1/2./p2) - (p0-y)/p2 );
+	if ( whichSol == 0 ) return -p1/2./p2 + sqrt( sq(p1/2./p2) - (p0-y)/p2 );
+	else                 return -p1/2./p2 - sqrt( sq(p1/2./p2) - (p0-y)/p2 );
 }
 
 ///
@@ -272,103 +272,103 @@ float CLIntervalMaker::pq(float p0, float p1, float p2, float y, int whichSol) c
 /// \return true, if inpterpolation was performed, false, if conditions were not met
 ///
 bool CLIntervalMaker::interpolatePol2fit(const TH1F* h, int i, float y, float central, bool upper,
-	float &val, float &err) const
+		float &val, float &err) const
 {
 	// cout << "CLIntervalMaker::interpolatePol2fit(): i=" << i << " y=" << y << " central=" << central << endl;
-  if ( !( 2 <= i && i <= h->GetNbinsX()-1 ) ) return false;
+	if ( !( 2 <= i && i <= h->GetNbinsX()-1 ) ) return false;
 	if ( binsOnSameSide(i-1, y) && binsOnSameSide(i, y) ){
 		cout << "CLIntervalMaker::interpolatePol2fit() : ERROR : bins i-1, i, and i+1 on same side of y" << endl;
 		return false;
 	}
 
-  // create a TGraph that we can fit
-  TGraph *g = new TGraph(3);
-  g->SetPoint(0, h->GetBinCenter(i-1), h->GetBinContent(i-1));
-  g->SetPoint(1, h->GetBinCenter(i),   h->GetBinContent(i));
-  g->SetPoint(2, h->GetBinCenter(i+1), h->GetBinContent(i+1));
+	// create a TGraph that we can fit
+	TGraph *g = new TGraph(3);
+	g->SetPoint(0, h->GetBinCenter(i-1), h->GetBinContent(i-1));
+	g->SetPoint(1, h->GetBinCenter(i),   h->GetBinContent(i));
+	g->SetPoint(2, h->GetBinCenter(i+1), h->GetBinContent(i+1));
 
-  // see if we can add a 4th and 5th point:
-  // add a point to the beginning
-  if ( 3 <= i && i <= h->GetNbinsX()-2 ){
-	  if ( (h->GetBinContent(i-2) < h->GetBinContent(i-1) && h->GetBinContent(i-1) < h->GetBinContent(i))
-	    || (h->GetBinContent(i-2) > h->GetBinContent(i-1) && h->GetBinContent(i-1) > h->GetBinContent(i)) ){
-	    TGraph *gNew = new TGraph(g->GetN()+1);
-	    gNew->SetPoint(0, h->GetBinCenter(i-2), h->GetBinContent(i-2));
-	    Double_t pointx, pointy;
-	    for ( int i=0; i<g->GetN(); i++ ){
-	      g->GetPoint(i, pointx, pointy);
-	      gNew->SetPoint(i+1, pointx, pointy);
-	    }
-	    delete g;
-	    g = gNew;
-	  }
-	  // add a point to the end
-	  if ( (h->GetBinContent(i+2) < h->GetBinContent(i+1) && h->GetBinContent(i+1) < h->GetBinContent(i))
-	    || (h->GetBinContent(i+2) > h->GetBinContent(i+1) && h->GetBinContent(i+1) > h->GetBinContent(i)) ){
-	    g->Set(g->GetN()+1);
-	    g->SetPoint(g->GetN()-1, h->GetBinCenter(i+2), h->GetBinContent(i+2));
-	  }
+	// see if we can add a 4th and 5th point:
+	// add a point to the beginning
+	if ( 3 <= i && i <= h->GetNbinsX()-2 ){
+		if ( (h->GetBinContent(i-2) < h->GetBinContent(i-1) && h->GetBinContent(i-1) < h->GetBinContent(i))
+				|| (h->GetBinContent(i-2) > h->GetBinContent(i-1) && h->GetBinContent(i-1) > h->GetBinContent(i)) ){
+			TGraph *gNew = new TGraph(g->GetN()+1);
+			gNew->SetPoint(0, h->GetBinCenter(i-2), h->GetBinContent(i-2));
+			Double_t pointx, pointy;
+			for ( int i=0; i<g->GetN(); i++ ){
+				g->GetPoint(i, pointx, pointy);
+				gNew->SetPoint(i+1, pointx, pointy);
+			}
+			delete g;
+			g = gNew;
+		}
+		// add a point to the end
+		if ( (h->GetBinContent(i+2) < h->GetBinContent(i+1) && h->GetBinContent(i+1) < h->GetBinContent(i))
+				|| (h->GetBinContent(i+2) > h->GetBinContent(i+1) && h->GetBinContent(i+1) > h->GetBinContent(i)) ){
+			g->Set(g->GetN()+1);
+			g->SetPoint(g->GetN()-1, h->GetBinCenter(i+2), h->GetBinContent(i+2));
+		}
 	}
 
-  // debug: show fitted 1-CL histogram
-  // if ( y<0.1 )
-  // // if ( methodName == TString("Plugin") && y<0.1 )
-  // {
-  //   // TString debugTitle = methodName + Form(" y=%.2f ",y);
-  //   // debugTitle += upper?Form("%f upper",central):Form("%f lower",central);
-  // 		TString debugTitle = "honk";
-  //   TCanvas *c = newNoWarnTCanvas(getUniqueRootName(), debugTitle);
-  //   g->SetMarkerStyle(3);
-  //   g->SetHistogram(const_cast<TH1F*>(h));
-  //   const_cast<TH1F*>(h)->Draw();
-  //   g->Draw("p");
-  // }
+	// debug: show fitted 1-CL histogram
+	// if ( y<0.1 )
+	// // if ( methodName == TString("Plugin") && y<0.1 )
+	// {
+	//   // TString debugTitle = methodName + Form(" y=%.2f ",y);
+	//   // debugTitle += upper?Form("%f upper",central):Form("%f lower",central);
+	// 		TString debugTitle = "honk";
+	//   TCanvas *c = newNoWarnTCanvas(getUniqueRootName(), debugTitle);
+	//   g->SetMarkerStyle(3);
+	//   g->SetHistogram(const_cast<TH1F*>(h));
+	//   const_cast<TH1F*>(h)->Draw();
+	//   g->Draw("p");
+	// }
 
 	// fit
-  TF1 *f1 = new TF1("f1", "pol2", h->GetBinCenter(i-2), h->GetBinCenter(i+2));
-  g->Fit("f1", "q");    // fit linear to get decent start parameters
-  g->Fit("f1", "qf+");  // refit with minuit to get more correct errors (TGraph fit errors bug)
-  float p[3], e[3];
-  for ( int ii=0; ii<3; ii++ ){
-    p[ii] = f1->GetParameter(ii);
-    e[ii] = f1->GetParError(ii);
-  }
+	TF1 *f1 = new TF1("f1", "pol2", h->GetBinCenter(i-2), h->GetBinCenter(i+2));
+	g->Fit("f1", "q");    // fit linear to get decent start parameters
+	g->Fit("f1", "qf+");  // refit with minuit to get more correct errors (TGraph fit errors bug)
+	float p[3], e[3];
+	for ( int ii=0; ii<3; ii++ ){
+		p[ii] = f1->GetParameter(ii);
+		e[ii] = f1->GetParError(ii);
+	}
 
 	// get solution by solving the pol2 for x
-  float sol0 = pq(p[0], p[1], p[2], y, 0);
-  float sol1 = pq(p[0], p[1], p[2], y, 1);
+	float sol0 = pq(p[0], p[1], p[2], y, 0);
+	float sol1 = pq(p[0], p[1], p[2], y, 1);
 
 	// decide which of both solutions to use based on the position of
 	// the central value
-  int useSol = 0;
-  if ( (sol0<central && sol1>central) || (sol1<central && sol0>central) ){
-    if ( upper ){
-      if ( sol0<sol1 ) useSol = 1;
-      else             useSol = 0;
-    }
-    else{
-      if ( sol0<sol1 ) useSol = 0;
-      else             useSol = 1;
-    }
-  }
-  else{
-    if ( fabs(h->GetBinCenter(i)-sol0) < fabs(h->GetBinCenter(i)-sol1) ) useSol = 0;
-    else useSol = 1;
-  }
-  if ( useSol==0 ) val = sol0;
-  else             val = sol1;
+	int useSol = 0;
+	if ( (sol0<central && sol1>central) || (sol1<central && sol0>central) ){
+		if ( upper ){
+			if ( sol0<sol1 ) useSol = 1;
+			else             useSol = 0;
+		}
+		else{
+			if ( sol0<sol1 ) useSol = 0;
+			else             useSol = 1;
+		}
+	}
+	else{
+		if ( fabs(h->GetBinCenter(i)-sol0) < fabs(h->GetBinCenter(i)-sol1) ) useSol = 0;
+		else useSol = 1;
+	}
+	if ( useSol==0 ) val = sol0;
+	else             val = sol1;
 
-  // try error propagation: sth is wrong in the formulae
-  // float err0 = TMath::Max(sq(val-pq(p[0]+e[0], p[1], p[2], y, useSol)), sq(val-pq(p[0]-e[0], p[1], p[2], y, useSol)));
-  // float err1 = TMath::Max(sq(val-pq(p[0], p[1]+e[1], p[2], y, useSol)), sq(val-pq(p[0], p[1]-e[1], p[2], y, useSol)));
-  // float err2 = TMath::Max(sq(val-pq(p[0], p[1], p[2]+e[2], y, useSol)), sq(val-pq(p[0], p[1], p[2]-e[2], y, useSol)));
-  // err = sqrt(err0+err1+err2);
-  // printf("%f %f %f\n", val, pq(p[0]+e[0], p[1], p[2], y, useSol), pq(p[0]-e[0], p[1], p[2], y, useSol));
-  // printf("%f %f %f\n", val, pq(p[0], p[1]+e[1], p[2], y, useSol), pq(p[0], p[1]-e[1], p[2], y, useSol));
-  // printf("%f %f %f\n", val, pq(p[0], p[1], p[2]+e[2], y, useSol), pq(p[0], p[1], p[2]-e[2], y, useSol));
-  err = 0.0;
+	// try error propagation: sth is wrong in the formulae
+	// float err0 = TMath::Max(sq(val-pq(p[0]+e[0], p[1], p[2], y, useSol)), sq(val-pq(p[0]-e[0], p[1], p[2], y, useSol)));
+	// float err1 = TMath::Max(sq(val-pq(p[0], p[1]+e[1], p[2], y, useSol)), sq(val-pq(p[0], p[1]-e[1], p[2], y, useSol)));
+	// float err2 = TMath::Max(sq(val-pq(p[0], p[1], p[2]+e[2], y, useSol)), sq(val-pq(p[0], p[1], p[2]-e[2], y, useSol)));
+	// err = sqrt(err0+err1+err2);
+	// printf("%f %f %f\n", val, pq(p[0]+e[0], p[1], p[2], y, useSol), pq(p[0]-e[0], p[1], p[2], y, useSol));
+	// printf("%f %f %f\n", val, pq(p[0], p[1]+e[1], p[2], y, useSol), pq(p[0], p[1]-e[1], p[2], y, useSol));
+	// printf("%f %f %f\n", val, pq(p[0], p[1], p[2]+e[2], y, useSol), pq(p[0], p[1], p[2]-e[2], y, useSol));
+	err = 0.0;
 	// delete g;
-  return true;
+	return true;
 }
 
 
