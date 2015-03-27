9b6a6409dbf1d5d09fc43900ce5a4d915964a7de
mkarbach <moritz.karbach@gmail.com>
2015-03-27 15:47:48 +0100

    indent

--- core/include/OneMinusClPlot.h
+++ core/include/OneMinusClPlot.h
@@ -18,23 +18,23 @@ using namespace std;
 
 class OneMinusClPlot : public OneMinusClPlotAbs
 {
-public:
-  OneMinusClPlot(OptParser *arg, TString name="c1", TString title="c1");
+	public:
+		OneMinusClPlot(OptParser *arg, TString name="c1", TString title="c1");
 
-  void            drawSolutions();
-  void            drawCLguideLines();
-  TGraph*         getGraph(MethodAbsScan* s, bool first=true, bool last=false, bool filled=true){return scan1dPlot(s,first,last,filled);};
-  inline TString  getName(){return name;};
-  inline void     setPluginMarkers(bool yesNo=true){plotPluginMarkers = yesNo;};
-  void            Draw();
+		void            drawSolutions();
+		void            drawCLguideLines();
+		TGraph*         getGraph(MethodAbsScan* s, bool first=true, bool last=false, bool filled=true){return scan1dPlot(s,first,last,filled);};
+		inline TString  getName(){return name;};
+		inline void     setPluginMarkers(bool yesNo=true){plotPluginMarkers = yesNo;};
+		void            Draw();
 
-private:
-  void            drawCLguideLine(float pvalue);
-  void            drawVerticalLine(float x, int color, int style);
-  TGraph*         scan1dPlot(MethodAbsScan* s, bool first, bool last, bool filled);
-  void            scan1dPlotSimple(MethodAbsScan* s, bool first);
+	private:
+		void            drawCLguideLine(float pvalue);
+		void            drawVerticalLine(float x, int color, int style);
+		TGraph*         scan1dPlot(MethodAbsScan* s, bool first, bool last, bool filled);
+		void            scan1dPlotSimple(MethodAbsScan* s, bool first);
 
-  bool            plotPluginMarkers;
+		bool            plotPluginMarkers;
 };
 
 #endif
