aa50fa49738ee46fe8c872eadb37886d4fe650c2
Matthew Kenzie <matthew.kenzie@me.com>
2017-04-07 16:25:16 +0100

    Added a bit more description to the tutorial

--- tutorial/main/tutorial_dataset.cpp
+++ tutorial/main/tutorial_dataset.cpp
@@ -20,10 +20,28 @@ int main(int argc, char* argv[])
   // How to run the tutorial:
   // bin/tutorial_dataset_build_workspace
 
-  // bin/tutorial_dataset --var branchingRatio --npoints 20 --scanrange 1e-7:2e-6
-  // bin/tutorial_dataset -a pluginbatch --var branchingRatio --ntoys 50  --npoints 20 --scanrange 1e-7:2e-6
-  // bin/tutorial_dataset -a plugin --var branchingRatio --npoints 20 --scanrange 1e-7:2e-6 -i
-
+  // 1.) Running a Profile Likelihood Scan
+  //        bin/tutorial_dataset --var branchingRatio --npoints 50 --scanrange 0.:1.e-6
+  // 2.) If you want to just remake the plot (without rescanning) add the -a plot option
+  //        bin/tutorial_dataset --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 -a plot
+  // 3.) If you want to add the CLs method add the option --cls 1
+  //        bin/tutorial_dataset --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 --cls 1
+  // 4.) To do a Feldman Cousins plugin scan (run a bunch in parallel and give them different names with --nrun %d
+  //        bin/tutorial_dataset -a pluginbatch --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 --ntoys 100 --nrun 1
+  //        bin/tutorial_dataset -a pluginbatch --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 --ntoys 100 --nrun 2
+  //        bin/tutorial_dataset -a pluginbatch --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 --ntoys 100 --nrun 3
+  //        bin/tutorial_dataset -a pluginbatch --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 --ntoys 100 --nrun 4
+  //        bin/tutorial_dataset -a pluginbatch --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 --ntoys 100 --nrun 5
+  // 5.) To read a bunch of Feldman Cousins scans back in (use the -j option to label the different run numbers)
+  //        bin/tutorial_dataset -a plugin --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 -j 1-5
+  // 6.) To just plot the Feldman Cousins stuff without having to re-scan or re-read add the -a plot option again
+  //        bin/tutorial_dataset -a plugin --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 -j 1-5 -a plot
+  // 7.) The F-C CLs method is a bit overkill (to do the classic CLs thing (with the FC toys) and plot the expected values as well) use the --cls 2 option (note you can pass --cls multiple times)
+  //        bin/tutorial_dataset -a plugin --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 -j 1-5 -a plot --cls 1 --cls 2
+  // 8.) There are various ways of prettyfying your plots - for CLs stuff you can try adding --qh 23 (moves the CL label) --group LHCb (adds LHCb label) --prelim (add preliminary label)
+  //        bin/tutorial_dataset -a plugin --var branchingRatio --npoints 50 --scanrange 0.:1.e-6 -j 1-5 -a plot --cls 1 --cls 2 --qh 23 --group LHCb --prelim
+  //
+  // If you have any problems contact Matthew Kenzie (matthew.kenzie@cern.ch) or Titus Mombacher (titus.mombacher@cern.ch)
 
   // Load the workspace from its file
   TFile f("workspace.root");
@@ -44,18 +62,21 @@ int main(int argc, char* argv[])
 	workspace->var("Nbkg")->setRange(4000,6000);
 
   // Construct the PDF and pass the workspace to it
+  //    note that you can write your own PDF_DatasetsTutorial Class which defines your own fitting procedure etc.
+  //    this should inherit from PDF_Datasets
+
   PDF_Datasets* pdf = new PDF_Datasets(workspace);
-  // PDF_Datasets* pdf = new PDF_DatasetTutorial(workspace);
+  // PDF_Datasets* pdf = new PDF_DatasetTutorial(workspace); // put your inherited fitter if you want to
   pdf->initData("data"); // this is the name of the dataset in the workspace
   pdf->initPDF("mass_model"); // this the name of the pdf in the workspace (without the constraints)
-  pdf->initBkgPDF("background_model"); // this the name of the pdf in the workspace (without the constraints)
+  pdf->initBkgPDF("background_model"); // this the name of the background pdf in the workspace (without the constraints)
   pdf->initObservables("datasetObservables"); // non-global observables whose measurements are stored in the dataset (for example the mass).
   pdf->initGlobalObservables("global_observables_set"); // global observables
   pdf->initParameters("parameters"); // all parameters
   pdf->initConstraints("constraint_set"); // RooArgSet containing the "constraint" PDF's
-  // optional (will not effect the results but just make some plots for you)
+  // the below are optional (will not effect the results but just make some plots for you)
   pdf->addFitObs("mass");                         // this is not required but will make some sanity plots
-  pdf->unblind("mass","[4360:5260],[5460:6360]"); // have to be careful about staying blind
+  pdf->unblind("mass","[4360:5260],[5460:6360]"); // have to be a bit careful about staying blind (this code isn't yet really blind friendly)
 
   // Start the Gammacombo Engine
   GammaComboEngine gc("tutorial_dataset", argc, argv);
@@ -68,5 +89,10 @@ int main(int argc, char* argv[])
 
   // Combiners are not supported when working with datsets.
   // The statistical model is fully defined with the PDF
+  // In some other use cases you will see lines like
+  // gc.newCombiner(1, "Combiner Name", "Combiner Title", 2,3,4 );
+  // these have no meaning in the datasets case
+
+  // now run it
   gc.run();
 }
