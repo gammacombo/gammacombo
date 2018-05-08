c944f3e555bf30484049a85e779ff273f5e17fa6
Titus Mombaecher <titus.mombacher@cern.ch>
2018-05-08 14:05:12 +0200

    modify batch script writer to allow also the datasets part

--- core/src/GammaComboEngine.cpp
+++ core/src/GammaComboEngine.cpp
@@ -1676,8 +1676,13 @@ void GammaComboEngine::setObservablesFromFile(Combiner *c, int cId)
 ///
 void GammaComboEngine::writebatchscripts()
 {
-  m_batchscriptwriter->writeScripts(arg, &cmb);
-  exit(0);
+	if (runOnDataSet){
+		m_batchscriptwriter->writeScripts_datasets(arg, getPdf(0));
+	}
+	else{
+		m_batchscriptwriter->writeScripts(arg, &cmb);
+	}
+  	exit(0);
 }
 
 ///
