4d2d75bd90785a14861223db1019a694305b99b2
Titus Mombächer <titus.mombacher@cern.ch>
2020-03-27 09:41:41 +0100

    improve smoothing for cls plots

--- core/src/OneMinusClPlot.cpp
+++ core/src/OneMinusClPlot.cpp
@@ -425,31 +425,31 @@ void OneMinusClPlot::scan1dCLsPlot(MethodAbsScan *s, bool smooth, bool obsError)
 
     //alternative smoothing option, needs more fiddling
     // gExp    = (TGraph*)smoother->SmoothKern( gExpRaw   ,"normal",hExp->GetBinWidth(1)*4)->Clone("gExp");
-    gErr1Up = (TGraph*)smoother->SmoothKern( gErr1UpRaw,"normal",hErr1Up->GetBinWidth(1)*2)->Clone("gErr1Up");
+    gErr1Up = (TGraph*)smoother->SmoothKern( gErr1UpRaw,"normal",hErr1Up->GetBinWidth(1)*2, hErr1Up->GetNbinsX())->Clone("gErr1Up");
     // gErr1Dn = (TGraph*)smoother->SmoothKern( gErr1DnRaw,"normal",hErr1Dn->GetBinWidth(1)*4)->Clone("gErr1Dn");
-    gErr2Up = (TGraph*)smoother->SmoothKern( gErr2UpRaw,"normal",hErr2Up->GetBinWidth(1)*2)->Clone("gErr2Up");
+    gErr2Up = (TGraph*)smoother->SmoothKern( gErr2UpRaw,"normal",hErr2Up->GetBinWidth(1)*2, hErr1Up->GetNbinsX())->Clone("gErr2Up");
     // gErr2Dn = (TGraph*)smoother->SmoothKern( gErr2DnRaw,"normal",hErr2Dn->GetBinWidth(1)*4)->Clone("gErr2Dn");
 
     // //make sure the CLs=1 points do NOT get smoothed away
 
-    // double *xvals = gExp->GetX();
-    // double *xvalsRaw = gExpRaw->GetX();
-    // double *yvalsRawExp = gExpRaw->GetY();
-    // double *yvalsRawExpErr1Up = gErr1UpRaw->GetY();
-    // double *yvalsRawExpErr2Up = gErr2UpRaw->GetY();
+    double *xvals = gExp->GetX();
+    double *xvalsRaw = gExpRaw->GetX();
+    double *yvalsRawExp = gExpRaw->GetY();
+    double *yvalsRawExpErr1Up = gErr1UpRaw->GetY();
+    double *yvalsRawExpErr2Up = gErr2UpRaw->GetY();
 
-    // for (int i=0; i<gExp->GetN(); i++){
-    // 	std::cout << xvalsRaw[i] << "\t" <<xvals[i] << std::endl;
-    // 	if(yvalsRawExp[i]>0.99){
-    // 		gExp->SetPoint(i,xvals[i], 1.0);
-    // 	}
-    // 	if(yvalsRawExpErr1Up[i]>0.99){
-    // 		gErr1Up->SetPoint(i,xvals[i], 1.0);
-    // 	}
-    // 	if(yvalsRawExpErr2Up[i]>0.99){
-    // 		gErr2Up->SetPoint(i,xvals[i], 1.0);
-    // 	}
-    // }
+    for (int i=0; i<gExp->GetN(); i++){
+    	std::cout << xvalsRaw[i] << "\t" <<xvals[i] << std::endl;
+    	// if(yvalsRawExp[i]>0.99){
+    	// 	gExp->SetPoint(i,xvals[i], 1.0);
+    	// }
+    	if(yvalsRawExpErr1Up[i]>0.99){
+    		gErr1Up->SetPoint(i,xvals[i], 1.0);
+    	}
+    	if(yvalsRawExpErr2Up[i]>0.99){
+    		gErr2Up->SetPoint(i,xvals[i], 1.0);
+    	}
+    }
 
     // fix point 0 to CLs=1 for all expected curves
 
