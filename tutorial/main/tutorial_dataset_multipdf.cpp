c98a390d4207e79460fad7657dfeae89c5335f97
Fionn Caitlin Ros Bishop <fionn.caitlin.ros.bishop@cern.ch>
2020-10-09 16:15:11 +0100

    Added comment in tutorial about comparing to behaviour with single pdf

--- tutorial/main/tutorial_dataset_multipdf.cpp
+++ tutorial/main/tutorial_dataset_multipdf.cpp
@@ -38,6 +38,7 @@ int main(int argc, char* argv[])
   PDF_Datasets* pdf = new PDF_Datasets(workspace);
   pdf->initData("data"); // this is the name of the dataset in the workspace
   pdf->initPDF("roomultipdf"); // this the name of the pdf in the workspace (without the constraints)
+  // To compare to the behaviour with a single pdf, you can instead use e.g. pdf->initPDF("SBmodel_exponential")
   pdf->initMultipdfCat("pdf_index"); // this is the only extra thing that needs initialising for a multipdf
   pdf->initObservables("datasetObservables"); // non-global observables whose measurements are stored in the dataset (for example the mass).
   pdf->initGlobalObservables("global_observables_set"); // global observables
